1451c3120fb8405061ee00654afdc182be4a24f1
Prabesh Sharma <prwshshrm@gmail.com>
2025-02-08 02:41:42 +0200

    Update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/CalculatorJs/"
+  base: "/CalculatorJs/",
+  build: {
+    outDir: 'dist', // Ensure this matches your GitHub Actions config
+  },
 })
